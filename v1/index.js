3745b48f0196bb0f63763c7ea8bcd7c993bfccb6
Ryan Efendy <ryanefendy95@gmail.com>
2018-10-21 12:45:44 -0700

    FIX: enable CORS and update response to client

--- v1/index.js
+++ v1/index.js
@@ -1,42 +1,44 @@
-var face = require('./face')
-var audio = require('./audio')
-var router = require('express').Router();
+var face = require("./face");
+var audio = require("./audio");
+var router = require("express").Router();
 //var bodyParser = require('body-parser');
 // router.use(bodyParser.raw())
 
-router.route('/face')
-  .post(function (req, res) {
-    let b64 = [];
-    req.on('data', (chunk) => {
+router.route("/face").post(function(req, res) {
+  let b64 = [];
+  req
+    .on("data", chunk => {
       b64.push(chunk);
-    }).on('end', () => {
+    })
+    .on("end", () => {
       b64 = Buffer.concat(b64).toString();
-      face.runRequest(b64, function (body) {
-        let data = JSON.parse(body);
-        data = data[0].faceAttributes.emotion;
-        console.log(data)
-        res.send(data) // only send first face
-      })
+      face.runRequest(b64, body => {
+        // let data = JSON.parse(body);
+        // data = data[0].faceAttributes.emotion;
+        // console.log(data)
+        res.header("Access-Control-Allow-Origin", "*"); // enable cross-origin resource sharing (CORS)
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.send(JSON.parse(body)); // only send first face
+      });
     });
+});
+router.route("/audio").get(function(req, res) {
+  console.log("audio");
+  const audioFile = "/Users/willtaylor/downloads/AngerDemo.mp3";
+  audio.runRequest(audioFile, function(data) {
+    let standardData = {};
+    for (i in data) {
+      if (data[i].emotion === "happy") {
+        data[i].emotion = "happiness";
+      } else if (data[i].emotion === "sad") {
+        data[i].emotion = "sadness";
+      } else if (data[i].emotion === "angry") {
+        data[i].emotion = "anger";
+      }
+      standardData[data[i].emotion] = data[i].score;
+    }
+    res.send(standardData);
   });
-router.route('/audio')
-  .get(function (req, res) {
-    console.log('audio');
-    const audioFile = "/Users/willtaylor/downloads/AngerDemo.mp3";
-    audio.runRequest(audioFile, function (data) {
-        let standardData = {};
-        for (i in data) {
-            if (data[i].emotion === 'happy') {
-                data[i].emotion = 'happiness';
-            } else if (data[i].emotion === 'sad') {
-                data[i].emotion = 'sadness';
-            } else if (data[i].emotion === 'angry') {
-                data[i].emotion = 'anger';
-            }
-            standardData[data[i].emotion] = data[i].score;
-        }
-        res.send(standardData);
-      })
-  });
+});
 
 module.exports = router;
